0d5431221f1c9a954ba8f1461d087f5121a3094a
Liskin-Inc <liskin.inc@yandex.ru>
2020-07-22 03:14:12 +0300

    Updates

---
+++ precache-manifest.dcef2433289d5d0519465296122aed21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a007f78352a58d74ad35fc9009719cac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f3430a02f9474377337",
+    "url": "/static/css/2.ed87aa75.chunk.css"
+  },
+  {
+    "revision": "9f3430a02f9474377337",
+    "url": "/static/js/2.2977e440.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2977e440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a005a2de3a639ddb30",
+    "url": "/static/js/main.8051828b.chunk.js"
+  },
+  {
+    "revision": "9ad8ac5643d24b5da40e",
+    "url": "/static/js/runtime-main.21c71093.js"
+  },
+  {
+    "revision": "befd82e5085d4ec0255a35715d3735ee",
+    "url": "/static/media/logoFull.befd82e5.png"
+  }
+]);
